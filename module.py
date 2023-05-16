7984fa41177d02aa1dfeaa344c1202da88295d5b
Frances Leigh <fleighbaay@gmail.com>
2023-05-16 16:10:26 +0800

    Pseudocode

---
+++ module.py
@@ -0,0 +1,21 @@
+# Create a module to call later 
+
+# Create class and list down attributes as stated in UML
+
+# Create methds for default constructor 
+
+# Methods for switch
+
+# Methods for channel
+    # Use conditional statement for limits in channel number
+
+# Methods for volume
+    # Use conditional statement for limits in channel number
+
+# Methods for channel control 
+    # Set channel value to none first
+    # Algorithm for adding and subtracting channel value
+
+# Methods for volume control
+    # Set volume value to none first
+    # Algorithm for adding and subtracting volume value
