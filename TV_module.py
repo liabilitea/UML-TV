82852a6cdfea430df210b5c8bf886d1f74370a4e
Frances Leigh <fleighbaay@gmail.com>
2023-05-16 22:56:38 +0800

    Variable and module rename

--- TV_module.py
+++ TV_module.py
@@ -5,22 +5,24 @@ class TV:
     channel: int
     volume: int
     on: bool
+    
 # Create methds for default constructor 
     def __init__(self, channel, volume):
         self.channel = channel
         self.volume = volume
+        
 # Methods for switch
-    def turnOn(self) -> None:
+    def turn_on(self) -> None:
         self.on = True
     
-    def turnOff(self) -> None:
+    def turn_off(self) -> None:
         self.off = False
  
 # Methods for channel
-    def getChannel(self) -> int:
+    def get_channel(self) -> int:
         return self.channel
     
-    def setChannel(self, channel) -> None:
+    def set_channel(self, channel) -> None:
     # Use conditional statement for limits in channel number
         if channel >= 1 and channel <= 120:
             self.channel = channel
@@ -28,16 +30,16 @@ class TV:
             print(f"Channel {channel} does not exist! Please try again.")
 
 # Methods for volume
-    def getVolume(self) -> int:
+    def get_volume(self) -> int:
         return self.volume
     
-    def setVolume(self, volume) -> None:
+    def set_volume(self, volume) -> None:
     # Use conditional statement for limits in channel number
         if volume >= 1 and volume <= 7:
             self.volume = volume
         else:
             print(f"Volume {volume} does not exit! Please try again.")
-            
+
 # Methods for channel control 
     # Set channel value to none first
     # Algorithm for adding and subtracting channel value
