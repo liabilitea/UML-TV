002e6fe414cdb1b2675c44012589cb392bdf080a
Frances Leigh <fleighbaay@gmail.com>
2023-05-16 22:54:17 +0800

    Methods for volume

--- module.py
+++ module.py
@@ -25,12 +25,19 @@ class TV:
         if channel >= 1 and channel <= 120:
             self.channel = channel
         else:
-            print(f"Channel {channel} does not exist! Please try again")
+            print(f"Channel {channel} does not exist! Please try again.")
 
 # Methods for volume
-    def getVolume()
+    def getVolume(self) -> int:
+        return self.volume
+    
+    def setVolume(self, volume) -> None:
     # Use conditional statement for limits in channel number
-
+        if volume >= 1 and volume <= 7:
+            self.volume = volume
+        else:
+            print(f"Volume {volume} does not exit! Please try again.")
+            
 # Methods for channel control 
     # Set channel value to none first
     # Algorithm for adding and subtracting channel value
