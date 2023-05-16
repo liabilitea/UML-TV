2caf5d12f45362270eb6664d492b02f066cb2420
Frances Leigh <fleighbaay@gmail.com>
2023-05-16 23:55:49 +0800

    Import and tv1

--- Test_TV.py
+++ Test_TV.py
@@ -1,7 +1,13 @@
 # Import the TV module
+import TV_module
 
 # Instantiate TV object from the TV_module.py
+
 # Create tv1
+tv1 = TV_module.TV();
+tv1.set_channel(30);
+tv1.set_volume(3);
+
 # Create tv2
 
 # Print 
